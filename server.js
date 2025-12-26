cef5ddb64b6ba80306e9d8ce258b32f0c3bc6b76
soumi-sd01 <soumi.debsingha01@gmail.com>
2025-12-27 01:00:05 +0530

    Remove HTML serving from backend

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const cors = require("cors");
 
 const path = require("path");
 const app = express();
-app.use(
-  cors({
-    origin: "https://gleeful-fox-551b22.netlify.app"
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
